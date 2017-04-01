8f419250f44a3e9b8f35d7b36de54f156e8cf493
Alice Jenno <alice@corporaterewards.co.uk>
2017-04-01 10:21:01 +0100

    #35 update to remove tracks and reload pages on add and remove tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -3,11 +3,13 @@ class TracksController < ApplicationController
 	def create	
 		track = RSpotify::Track.find([params[:uid]])
 		playlist.add_tracks!(track)
+		redirect_to root_url
 	end
 
 	def destroy	
-      track = RSpotify::Track.find(params[:uid])
-      playlist.remove_tracks!([track])
+      track = RSpotify::Track.find([params[:uid]])
+      playlist.remove_tracks!(track)
+      redirect_to root_url
 	end
 
 end
