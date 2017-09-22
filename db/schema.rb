3313262d91675bd918ef95437ed5f767d5c7ad81
cr-abednorz <alexander.bednorz@corporaterewards.co.uk>
2017-09-22 11:58:56 +0100

    modified:   app/controllers/tracks_controller.rb

--- db/schema.rb
+++ db/schema.rb
@@ -40,8 +40,8 @@ ActiveRecord::Schema.define(version: 20170504160204) do
   create_table "votes", force: :cascade do |t|
     t.integer  "track_id"
     t.boolean  "vote"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["track_id"], name: "index_votes_on_track_id"
   end
 
