9acf3d16850848518ee0b7031ab0ff5f66802d3d
Gillian Manning <gillian.manning@corporaterewards.co.uk>
2017-10-16 00:39:52 +0100

    refactor vote sorting away from sql and add vote counts to tracks model

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,26 +1,25 @@
 class TracksController < ApplicationController
 
   def create
-        track = RSpotify::Track.find([params[:uid]])
-        playlist.add_tracks!(track)
-        add_track = Track.create(:name => track[0].name, :artist => track[0].artists[0].name, :track_id => track[0].id)
-        track_id = Track.where(track_id: track[0].id).first
-        track_id.votes.create(vote: params[:vote])
-        redirect_to root_url
-    end
+    track = RSpotify::Track.find([params[:uid]])
+    playlist.add_tracks!(track)
+    add_track = Track.create(:name => track[0].name, :artist => track[0].artists[0].name, :track_id => track[0].id)
+    track_id = Track.where(track_id: track[0].id).first
+    track_id.votes.create(vote: params[:vote])
+    redirect_to root_url
+  end
 
-    def show
-        thisQuery = File.read(Rails.root.join('db', 'queries', 'tuneUp_topVotedTracks.sql').to_s).to_s
-        @playlist = ActiveRecord::Base.connection.execute(thisQuery)
-        @currently_playing = spotify_user.currently_playing
-        date = DateTime.parse(spotify_user.currently_playing.album.release_date)
-        @released_date = date.strftime('%b %d %Y')      
-    end
+  def show
+    @playlist = Track.all.sort_by {|track| track.sum_total_votes }.reverse
+    @currently_playing = spotify_user.currently_playing
+    date = DateTime.parse(spotify_user.currently_playing.album.release_date)
+    @released_date = date.strftime('%b %d %Y')      
+  end
 
-    def destroy
-        track = RSpotify::Track.find([params[:uid]])
-        playlist.remove_tracks!(track)
-        redirect_to root_url
-    end
+  def destroy
+    track = RSpotify::Track.find([params[:uid]])
+    playlist.remove_tracks!(track)
+    redirect_to root_url
+  end
 
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,3 +1,16 @@
 class Track < ApplicationRecord
   has_many :votes
+
+  def num_up_votes
+    self.votes.where(:vote => true).count
+  end
+
+  def num_down_votes
+    self.votes.where(:vote => false).count 
+  end
+
+  def sum_total_votes
+    self.votes.where(:vote => true).count - self.votes.where(:vote => false).count
+  end
+
 end
