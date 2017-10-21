fa7bc9688e9f31622ec2f9947bb9a7017495f4f6
Gillian Manning <gillian.manning@corporaterewards.co.uk>
2017-10-21 16:59:51 +0100

    Don't add another track entry if track already exists, just create up vote

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -2,10 +2,15 @@ class TracksController < ApplicationController
 
   def create
     track = RSpotify::Track.find([params[:uid]])
-    playlist.add_tracks!(track)
-    add_track = Track.create(:name => track[0].name, :artist => track[0].artists[0].name, :track_id => track[0].id)
-    track_id = Track.where(track_id: track[0].id).first
-    track_id.votes.create(vote: params[:vote])
+    if !Track.exists?(track_id: track[0].id)
+      playlist.add_tracks!(track)
+      add_track = Track.create(:name => track[0].name, :artist => track[0].artists[0].name, :track_id => track[0].id)
+      track_id = Track.where(track_id: track[0].id).first
+      track_id.votes.create(vote: params[:vote])
+    else
+      track_id = Track.where(track_id: track[0].id).first
+      track_id.votes.create(vote: params[:vote])
+    end
     redirect_to root_url
   end
 
