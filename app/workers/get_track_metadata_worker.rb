3925550af14897a25d9c768c1f67ecdf9215c23e
Gillian Manning <gillian.manning@corporaterewards.co.uk>
2017-11-27 22:39:25 +0000

    add sidekiq and create job to fetch track uri and metadata if missing

---
+++ app/workers/get_track_metadata_worker.rb
@@ -0,0 +1,16 @@
+class GetTrackMetadataWorker
+  include Sidekiq::Worker
+
+  def perform
+    tracks = Track.where(:metadata => nil).pluck(:track_id)
+
+    tracks.each do |track_id|
+      puts track_id
+      spotify_track = RSpotify::Track.find(track_id)
+      track = Track.where(:track_id => track_id).first
+      track.uri = spotify_track.uri
+      track.metadata = spotify_track
+      track.save
+    end
+  end
+end
