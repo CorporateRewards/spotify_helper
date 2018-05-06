f44f510be05e5162e08a5a071b6c9067d5b4c365
Gillian Manning <gillian.manning@corporaterewards.co.uk>
2018-05-06 17:44:34 +0100

    Move some logic out of application controller Some altertions to Track model and controller Add individual track play, next track, and previous track controls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,28 +2,17 @@ class ApplicationController < ActionController::Base
   require 'httparty'
   require 'base64'
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :currently_playing, except: [:spotify]
+  before_action :authenticate_user!, :currently_playing
+  # , except: [:spotify]
 
   def playlist
     @playlist = RSpotify::Playlist.find('crtechteam', '5esgCdY5baXWpIrPHs5ZYp')
 
   end
 
-
-  def spotify
-    session[:sp_user] = request.env['omniauth.auth']
-    suser = RSpotify::User.new(session[:sp_user])
-    user_hash = suser.to_hash
-    spotify_auth = SpotifyAuth.create(:sp_user_hash => user_hash)
-    @spotify_user = suser
-    user_auth = SpotifyAuth.last
-    @userauth = user_auth.sp_user_hash
-    redirect_to root_url
-  end
-
   def spotify_user
     if SpotifyAuth.last
-      refresh_access
+      # refresh_access
       user_auth = SpotifyAuth.last
       @userauth = user_auth.sp_user_hash
       RSpotify::User.new(@userauth)
@@ -49,7 +38,6 @@ class ApplicationController < ActionController::Base
       @user_auth.save
   end
   end
-
 
   def currently_playing
     begin
