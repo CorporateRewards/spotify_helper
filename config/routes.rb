d03ba0276c26c87dc8170621fb5abfeefef95102
Gillian Manning <gillian.manning@corporaterewards.co.uk>
2018-06-22 11:11:25 +0100

    homepage redesign

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :users
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tracks#show'
+  root to: 'pages#welcome'
   resource :search, only: [:show]
   resource :tracks do
     resource :votes
