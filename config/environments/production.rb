a018b915f9f772ebd8be2fddabfe0dcb07b0e9f3
Gillian Manning <gillian.manning@corporaterewards.co.uk>
2018-06-24 23:08:28 +0100

    sendgrid setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
  # Setup the mailer config
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'spotify-helper-crtech.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
