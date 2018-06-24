339fe667f6bde6879f91ca1af82d7ee3558180c6
Gillian Manning <gillian.manning@corporaterewards.co.uk>
2018-06-24 23:03:48 +0100

    sendgrid setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,4 +56,15 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   # Defines default URL options
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'spotify-helper-crtech.herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
