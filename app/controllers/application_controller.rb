703be704526096c16c10d5b7b2b15c9d8e31d03d
Gill Manning <gill@createk.io>
2018-11-30 14:53:21 +0000

    Pleasing rubocop mostly

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,50 +9,56 @@ class ApplicationController < ActionController::Base
   end
 
   def authenticate_user!(*args)
-    if admin_signed_in?
-      return
-    end
-    super
+    return super unless admin_signed_in?
   end
 
   def spotify_user
-    if SpotifyAuth.last
-      # refresh_access
-      user_auth = SpotifyAuth.last
-      @userauth = user_auth.sp_user_hash
-      @spotify_user = RSpotify::User.new(@userauth)
-    end
+    return unless SpotifyAuth.last
+
+    # refresh_access
+    user_auth = SpotifyAuth.last
+    @userauth = user_auth.sp_user_hash
+    @spotify_user = RSpotify::User.new(@userauth)
+  end
+
+  def user
+    return unless current_user || current_admin
+
+    @user = current_user || User.find_by(email: current_admin.email)
   end
 
   def refresh_access
-    if SpotifyAuth.last
-      client_id = ENV["spotify_id"]
-      client_secret = ENV["spotify_secret"]
-      @baseuser = Base64.strict_encode64("#{client_id}:#{client_secret}")
-      @user_auth = SpotifyAuth.last
-      @ref_token = @user_auth.sp_user_hash["credentials"].refresh_token
+    return unless SpotifyAuth.last
 
-      @urlstring_to_post = "https://accounts.spotify.com/api/token"
-      @result = HTTParty.post(@urlstring_to_post.to_str,
-        :body => {
-          :grant_type => "refresh_token",
-          :refresh_token => @ref_token
-        },
-        :headers => { 'Authorization' => 'Basic ODljNWFiYjA1YmQ0NDRlZGE3OThhZTJjMTVjY2I5MjE6N2I5YWJiMjNhZjhjNGRlM2E0NjQyZGE5MzcwN2M4MTU=' })
-      @user_auth.sp_user_hash["credentials"].token = @result["access_token"]
-      @user_auth.save
-  end
+    client_id = ENV['spotify_id']
+    client_secret = ENV['spotify_secret']
+    @baseuser = Base64.strict_encode64("#{client_id}:#{client_secret}")
+    @user_auth = SpotifyAuth.last
+    @ref_token = @user_auth.sp_user_hash['credentials'].refresh_token
+
+    @urlstring_to_post = 'https://accounts.spotify.com/api/token'
+    @result = HTTParty.post(
+      @urlstring_to_post.to_str,
+      body: {
+        grant_type: 'refresh_token',
+        refresh_token: @ref_token
+      },
+      headers: {
+        'Authorization' => 'Basic ODljNWFiYjA1YmQ0NDRlZGE3OThhZTJjMTVjY2I5MjE6N2I5YWJiMjNhZjhjNGRlM2E0NjQyZGE5MzcwN2M4MTU='
+      }
+    )
+    @user_auth.sp_user_hash['credentials'].token = @result['access_token']
+    @user_auth.save
   end
 
   def track_progress
-      @progress = spotify_user.player.progress
+    @progress = spotify_user.player.progress
     respond_to do |format|
       format.json { render json: @progress }
     end
   end
 
   def currently_playing
-
     begin
       if !spotify_user.nil?
         @player = spotify_user.player
@@ -64,13 +70,13 @@ class ApplicationController < ActionController::Base
     rescue
       @currently_playing = nil
     end
-    @user = current_user ? current_user : User.find_by(email: current_admin.email)
-    @votes = @user.votes.all
+
+    @votes = user.votes.all || nil if user
+
     respond_to do |format|
       format.html
       format.json { render json: @currently_playing }
       format.js
     end
   end
-
 end
--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,19 +1,17 @@
 class TracksController < ApplicationController
+  before_action :refresh_access, only: %i[next_track previous_track device_list play_individual_track]
 
-  before_action :refresh_access, only: [:next_track, :previous_track, :device_list, :play_individual_track]
-  def user
-    user ||= RSpotify::User.find('crtechteam')
-
+  def spotify_user
+    RSpotify::User.find('crtechteam')
   end
 
   def player
-    player ||= user.player
+    spotify_user.player
   end
 
-
   def user_auth
-      @user = SpotifyAuth.last
-      @user_auth = @user.sp_user_hash["credentials"].token
+    @user = SpotifyAuth.last
+    @user_auth = @user.sp_user_hash['credentials'].token
   end
 
   def create
@@ -31,26 +29,26 @@ class TracksController < ApplicationController
     # See if track is in database
     track = Track.find_or_initialize_by(track_id: spotify_track[0].id)
     playlist.add_tracks!(spotify_track)
-    track.update!(name: spotify_track[0].name,
+    track.update!(
+      name: spotify_track[0].name,
       artist: spotify_track[0].artists[0].name,
       track_id: spotify_track[0].id,
       uri: spotify_track[0].uri,
       metadata: spotify_track
     )
-    user = current_user ? current_user : User.find_by(email: current_admin.email)
+    # user = current_user ? current_user : User.find_by(email: current_admin.email)
     vote = track.votes.find_or_create_by(user: user)
     if vote.update(vote: params[:vote])
       respond_to :js
     else
-      flash.notice = "Sorry, something went wrong, try again please"
+      flash.notice = 'Sorry, something went wrong, try again please'
       redirect_to :back
     end
-    #redirect_to root_url
   end
 
   def index
     @playlist = Track.sorted_by_most_votes
-    @user = current_user ? current_user : User.find_by(email: current_admin.email)
+    # @user = current_user ? current_user : User.find_by(email: current_admin.email)
     @votes = @user.votes.all
   end
 
@@ -66,23 +64,23 @@ class TracksController < ApplicationController
 
   def update_playlist
     @alltracks = Track.made_the_playlist
-    if !@alltracks.empty?
-      if @alltracks.length < 100
-        @playlist = RSpotify::Playlist.find('crtechteam', '5esgCdY5baXWpIrPHs5ZYp')
-        @playlist.replace_tracks!(@alltracks.flatten)
-      else
-        @first_100_tracks = @alltracks.first(100)
-        @playlist = RSpotify::Playlist.find('crtechteam', '5esgCdY5baXWpIrPHs5ZYp')
-        @playlist.replace_tracks!(@first_100_tracks.flatten)
-        length = @alltracks.length - 100
-        @additional = @alltracks.last(length)
+    return if @alltracks.empty?
 
-        @additional.each do |t|
-          @playlist.add_tracks!(t)
-        end
+    if @alltracks.length < 100
+      @playlist = RSpotify::Playlist.find('crtechteam', '5esgCdY5baXWpIrPHs5ZYp')
+      @playlist.replace_tracks!(@alltracks.flatten)
+    else
+      @first_100_tracks = @alltracks.first(100)
+      @playlist = RSpotify::Playlist.find('crtechteam', '5esgCdY5baXWpIrPHs5ZYp')
+      @playlist.replace_tracks!(@first_100_tracks.flatten)
+      length = @alltracks.length - 100
+      @additional = @alltracks.last(length)
+
+      @additional.each do |t|
+        @playlist.add_tracks!(t)
+      end
     end
-      redirect_to root_url
-    end
+    redirect_to root_url
   end
 
   def destroy
@@ -92,21 +90,22 @@ class TracksController < ApplicationController
   end
 
   def device_list
-    @urlstring_to_post = "https://api.spotify.com/v1/me/player/devices"
-    @result = HTTParty.get(@urlstring_to_post.to_str,
-      :body => {
+    @urlstring_to_post = 'https://api.spotify.com/v1/me/player/devices'
+    @result = HTTParty.get(
+      @urlstring_to_post.to_str,
+      body: {
       },
-      :headers => { "Authorization" => "Authorization: Bearer #{user_auth}" })
+      headers: { 'Authorization' => "Authorization: Bearer #{user_auth}" }
+    )
   end
 
   def play_tracks
-    player.play()
+    player.play
     redirect_to root_url
   end
 
-
   def pause_tracks
-    player.pause()
+    player.pause
     redirect_to root_url
   end
 
@@ -117,34 +116,37 @@ class TracksController < ApplicationController
 
   def next_track
     @urlstring_to_post = 'https://api.spotify.com/v1/me/player/next'
-    @result = HTTParty.post(@urlstring_to_post.to_str,
-                            body: {},
-                            headers: { 'Authorization' => "Authorization: Bearer #{user_auth}" }
-                           )
-    @currently_playing = @player.currently_playing
-    @user = current_user ? current_user : User.find_by(email: current_admin.email)
-    @votes = @user.votes.all
+    @result = HTTParty.post(
+      @urlstring_to_post.to_str,
+      body: {},
+      headers: { 'Authorization' => "Authorization: Bearer #{user_auth}" }
+    )
+    @currently_playing = player.currently_playing
+    # @user = current_user ? current_user : User.find_by(email: current_admin.email)
+    @votes = user.votes.all
     respond_to :js
   end
 
   def previous_track
-    @urlstring_to_post = "https://api.spotify.com/v1/me/player/previous"
-    @result = HTTParty.post(@urlstring_to_post.to_str,
-      :body => {
+    @urlstring_to_post = 'https://api.spotify.com/v1/me/player/previous'
+    @result = HTTParty.post(
+      @urlstring_to_post.to_str,
+      body: {
       },
-      :headers => { "Authorization" => "Authorization: Bearer #{user_auth}" })
+      headers: { 'Authorization' => "Authorization: Bearer #{user_auth}" }
+    )
     redirect_to root_url
   end
 
   def volume_control
     volume = player.instance_variable_get(:@device).instance_variable_get(:@volume_percent).to_i
-    if params[:change] == "1"
+    if params[:change] == '1'
       volume += 10
-      player.volume(volume)
     else
       volume -= 10
-      player.volume(volume)
     end
+    player.volume(volume)
+
   end
 
 end
