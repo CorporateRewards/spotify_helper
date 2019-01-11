19d963dab52a7ddeaabedaa377fc985a720bbd02
Gill Manning <gill@createk.io>
2019-01-11 14:21:58 +0000

    Add all spotify permissions

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,24 @@ require 'rspotify/oauth'
 # GET https://accounts.spotify.com/authorize/?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV["spotify_id"], ENV["spotify_secret"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify playlist-modify-private user-top-read user-read-currently-playing user-read-playback-state user-read-recently-played user-modify-playback-state'
+  provider :spotify, ENV['spotify_id'], ENV['spotify_secret'],
+           scope: '
+            playlist-read-private
+            playlist-modify-private
+            playlist-modify-public
+            playlist-read-collaborative
+            user-modify-playback-state
+            user-read-currently-playing
+            user-read-playback-state
+            user-top-read
+            user-read-recently-played
+            app-remote-control
+            streaming
+            user-read-birthdate
+            user-read-email
+            user-read-private
+            user-follow-read
+            user-follow-modify
+            user-library-modify
+            user-library-read'
 end
-
-
