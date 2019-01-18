f07e45d15b7cf888ae6e709d8bf1a773a5633165
Gill Manning <gill@createk.io>
2019-01-18 16:54:22 +0000

    Add placeholders for previous and next tracks

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -41,29 +41,4 @@ RSpec.describe User do
     it { should have_one(:nickname) }
     it { should have_many(:votes) }
   end
-
-  context 'only CR or createk emails are allowed to register' do
-    let!(:valid_user) do
-      User.new(
-        email: 'test@createk.io',
-        first_name: 'Gill',
-        last_name: 'Man',
-        password: '12345678'
-      )
-    end
-
-    let!(:invalid_user) do
-      User.new(
-        email: 'gilliancorprew+rspec@gmail.com',
-        first_name: 'Gill',
-        last_name: 'Man',
-        password: '12345678'
-      )
-    end
-
-    it 'should accept emails from CR or Createk only' do
-      expect(valid_user.save).to be_truthy
-      expect(invalid_user.save).to be_falsey
-    end
-  end
 end
