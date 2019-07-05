04ed862335ab760421c47dad62d99f7222bfac32
Gill Manning <gill@createk.io>
2019-07-05 15:54:19 +0100

    create helper for determining which vote type to be active

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,10 @@
 module TracksHelper
+  def users_vote_on_track(track, vote)
+    track_vote = @user.votes.where(track_id: track.id, vote: vote)
+    if track_vote.present?
+      'disabled'
+    else
+      'active'
+    end
+  end
 end
