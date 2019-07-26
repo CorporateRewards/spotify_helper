8a8a743e91a948c7f78b35dc14b9d6ce532e713c
Gill Manning <gill@createk.io>
2019-07-26 14:18:11 +0100

    initiate rspotify user before updating tracks and extract to private methods

--- app/workers/update_playlist_worker.rb
+++ app/workers/update_playlist_worker.rb
@@ -4,11 +4,22 @@ class UpdatePlaylistWorker
   def perform
     return if tracks_to_be_added.empty?
 
-    playlist = RSpotify::Playlist.find('crtechteam', '5esgCdY5baXWpIrPHs5ZYp')
-    playlist.replace_tracks!(tracks_to_be_added.flatten)
+    initiate_rspotify_user
+    initiate_playlist
+    @playlist.replace_tracks!(tracks_to_be_added.flatten)
   end
+
+  private
 
   def tracks_to_be_added
     Track.top_voted_tracks + Track.recently_added_tracks
+  end
+
+  def initiate_rspotify_user
+    RSpotify::User.new(SpotifyAuth.last.sp_user_hash)
+  end
+
+  def initiate_playlist
+    @playlist = RSpotify::Playlist.find('crtechteam', '5esgCdY5baXWpIrPHs5ZYp')
   end
 end
