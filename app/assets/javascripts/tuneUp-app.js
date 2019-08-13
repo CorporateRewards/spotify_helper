61431010581a2adc8862a846a1456f1da7aa08d0
Gill Manning <gill@createk.io>
2019-08-13 19:45:10 +0100

    add previous and close buttons to search results

--- app/assets/javascripts/tuneUp-app.js
+++ app/assets/javascripts/tuneUp-app.js
@@ -1,35 +1,39 @@
 TUNEUP = {
-    common: {
-        init: function () {
-          var searchform = $('#search-form');
-          var search = $('#search');
-          var nextresults = $('#next-results');
+  common: {
+    init: function() {
+      var searchform = $("#search-form");
+      var search = $("#search");
+      var nextresults = $("#next-results");
+      var closeresults = $("#close-search");
 
-          // Set page count to 0 and perform initial search on search form submission //
-          //////////////////////////////////////////////////////////////////////////////
+      // Set page count to 0 and perform initial search on search form submission //
+      //////////////////////////////////////////////////////////////////////////////
 
-          searchform.on( "submit", function(e) {
-            e.preventDefault();
-            localStorage.pageCount = 0;
+      searchform.on("submit", function(e) {
+        e.preventDefault();
+        localStorage.pageCount = 0;
 
-          // Only send the request if the search has a value
-            if(search.val() == "") {
-              localStorage.pageCount = 'undefined';
-              console.log("no search");
-              $("#results").html(""); 
-            } else {
-              $.get("/search",
-                {
-                  search: search.val(), 
-                  offset: localStorage.pageCount
-                }, 
-                function( data ) {
-                  // console.log(data);
-                  $("#results").html( data );
-                })
-              } 
-            });
-          }
+        // Only send the request if the search has a value
+        if (search.val() == "") {
+          localStorage.pageCount = "undefined";
+          console.log("no search");
+          $("#search-results").html("");
+        } else {
+          $.get(
+            "/search",
+            {
+              search: search.val(),
+              offset: localStorage.pageCount
+            },
+            function(data) {
+              // console.log(data);
+              $("#recommended").addClass('hidden')
+              $("#search-results").html(data);
+              $('#previous-results').addClass('hidden');
+            }
+          );
         }
-      }
-
+      });
+    }
+  }
+};
