177c284fbe75ea5ff240f50bf7a03a9ea7c91049
Gill Manning <gill@createk.io>
2019-08-14 18:50:38 +0100

    don't authenticate rspotify if spotify id and secret aren't present

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,16 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+# require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -27,8 +27,9 @@ module SpotifyHelper
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
   end
 
 end
-RSpotify.authenticate(ENV["spotify_id"], ENV["spotify_secret"])
+
+RSpotify.authenticate(ENV['spotify_id'], ENV['spotify_secret']) if ENV['spotify_id'].present? && ENV['spotify_secret'].present?
