e4b62ee8f1cf30c984e00dc7ee6219916fdf785a
Gill Manning <gill@createk.io>
2019-08-16 14:46:04 +0100

    populate tracks based on current playlist state

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@ phrases = BannedPhrases.create(
 # Create admin user
 Admin.create(email: 'gill@createk.io', password: 'password')
 User.create(email: 'gill@createk.io', password: 'password', first_name: 'gill', last_name: 'manning')
+
+# Update tracks from current playlist
+playlist = RSpotify::Playlist.find(ENV['spotify_user'], ENV['spotify_playlist'])
+playlist.tracks_cache.each do |playlist_track|
+  track = Track.find_or_initialize_by(track_id: playlist_track.id)
+  track.update!(
+    name: playlist_track.name,
+    artist: playlist_track.artists.map(&:name).join(', '),
+    track_id: playlist_track.id,
+    uri: playlist_track.uri,
+    metadata: playlist_track
+  )
+end
